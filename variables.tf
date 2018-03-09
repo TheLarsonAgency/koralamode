f1a92aa495b6bf6806a8d38d37ef5ca6f4d551e7
Bobby <karma0@gmail.com>
2018-03-09 15:01:47 -0700

    Updated to latest AMIs

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "ami_id" {
   description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under ami/nomad-consul.json. If no AMI is specified, the template will 'just work' by using the example public AMIs. WARNING! Do not use the example AMIs in a production setting!"
-  default = "ami-799e8f03"
+  default = "ami-afd228d2" # ubuntu16-ami
+  #default = "ami-10ca306d" # amazon-linux-ami
 }
 
 variable "aws_region" {
