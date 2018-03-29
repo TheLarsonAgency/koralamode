0dde320dec3a351b3be83d4a0d00f903a5a74738
Bobby <karma0@gmail.com>
2018-03-29 15:53:31 -0600

    Added CA install to images

--- ami/setup_nomad_consul_vault.sh
+++ ami/setup_nomad_consul_vault.sh
@@ -2,10 +2,18 @@
 set -e
 
 # Environment variables are set by packer
-/tmp/aws-koralamode/modules/install-nomad/install-nomad --version "${NOMAD_VERSION}"
 
+# Install Nomad
+git clone --branch "${NOMAD_MODULE_VERSION}" https://github.com/hashicorp/terraform-aws-nomad.git /tmp/terraform-aws-nomad
+/tmp/terraform-aws-nomad/modules/install-nomad/install-nomad --version "${NOMAD_VERSION}"
+
+# Install Vault
 git clone --branch "${VAULT_MODULE_VERSION}" https://github.com/hashicorp/terraform-aws-vault.git /tmp/terraform-aws-vault
 /tmp/terraform-aws-vault/modules/install-vault/install-vault --version "${VAULT_VERSION}"
 
+# Install Consul
 git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
 /tmp/terraform-aws-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
+
+# Add the Vault CA
+/tmp/terraform-aws-vault/modules/update-certificate-script/update-certificate-script --cert-file-path /opt/vault/tls/ca.cert.pem
