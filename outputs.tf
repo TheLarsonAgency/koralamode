b9834007562d9bc153b54a04fa3c49120fa3515b
Bobby <karma0@gmail.com>
2018-03-29 14:46:56 -0600

    Added ecr registry

--- outputs.tf
+++ outputs.tf
@@ -101,3 +101,15 @@ output "vault_fully_qualified_domain_name" {
 output "vault_elb_dns_name" {
   value = "${module.vault_elb.load_balancer_dns_name}"
 }
+
+output "ecr_registry_id" {
+  value = "${module.ecr.registry_id}"
+}
+
+output "ecr_registry_url" {
+  value = "${module.ecr.registry_url}"
+}
+
+output "ecr_role_name" {
+  value = "${module.ecr.role_name}"
+}
