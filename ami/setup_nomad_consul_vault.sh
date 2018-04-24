c82e436862b053eb145319828ef7bba0d8a01f3c
Bobby <karma0@gmail.com>
2018-04-23 18:10:44 -0600

    Added encrypted certs to AMI

--- ami/setup_nomad_consul_vault.sh
+++ ami/setup_nomad_consul_vault.sh
@@ -15,5 +15,9 @@ git clone --branch "${VAULT_MODULE_VERSION}" https://github.com/hashicorp/terraf
 git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
 /tmp/terraform-aws-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
 
+# Download, decrypt, and install certs
+wget -O - https://s3.amazonaws.com/koralamode-vault/certs.tar.gz.enc | base64 -d > certs.tar.gz.enc
+aws kms decrypt --region us-east-1 --ciphertext-blob fileb://certs.tar.gz.enc --output text --query Plaintext | base64 -d | gzip -d | sudo tar x -C /opt/vault/tls/
+
 # Add the Vault CA
-/tmp/terraform-aws-vault/modules/update-certificate-script/update-certificate-script --cert-file-path /opt/vault/tls/ca.cert.pem
+/tmp/terraform-aws-vault/modules/update-certificate-store/update-certificate-store --cert-file /opt/vault/tls/ca.cert.pem
