27d64a49aeb4b33b249db9852c6ad5fec92d2458
Bobby <karma0@gmail.com>
2018-04-25 17:46:03 -0600

    Added NAT gateway for outbound traffic from private subnets

--- vpc.tf
+++ vpc.tf
@@ -9,9 +9,8 @@ module "vpc" {
   private_subnets     = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
   public_subnets      = ["10.10.11.0/24", "10.10.12.0/24", "10.10.13.0/24"]
 
-  enable_nat_gateway = false
-  #enable_nat_gateway = true
-  #single_nat_gateway = true
+  enable_nat_gateway = true
+  single_nat_gateway = true
 
   #enable_s3_endpoint       = true
   #enable_dynamodb_endpoint = true
