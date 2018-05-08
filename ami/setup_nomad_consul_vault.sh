eeb23275629e2806373703e72ca337ad0cdcc0c2
Bobby <karmma0@gmail.com>
2018-05-07 22:55:06 -0600

    AMI build scripts enable nomad/vault integration

--- ami/setup_nomad_consul_vault.sh
+++ ami/setup_nomad_consul_vault.sh
@@ -9,11 +9,12 @@ git clone --branch "${NOMAD_MODULE_VERSION}" https://github.com/hashicorp/terraf
 
 sudo tee /opt/nomad/config/vault.hcl << EOF
 vault {
-  enabled = true
-  address = "https://vault.coolly.ninja:8200"
+  enabled   = true
+  cert_file = "/opt/nomad/tls/vault.crt.pem"
+  key_file  = "/opt/nomad/tls/vault.key.pem"
+  create_from_role = "nomad-cluster"
 }
 EOF
-sudo chown nomad:nomad /opt/nomad/config/vault.hcl
 
 # Install Vault
 git clone --branch "${VAULT_MODULE_VERSION}" https://github.com/hashicorp/terraform-aws-vault.git /tmp/terraform-aws-vault
@@ -27,6 +28,8 @@ git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terr
 wget -O - https://s3.amazonaws.com/koralamode-vault/certs.tar.gz.enc | base64 -d > certs.tar.gz.enc
 aws kms decrypt --region us-east-1 --ciphertext-blob fileb://certs.tar.gz.enc --output text --query Plaintext | base64 -d | gzip -d | sudo tar x -C /opt/vault/tls/
 sudo chown -R vault:vault /opt/vault/tls
+sudo cp -a /opt/vault/tls /opt/nomad/
+sudo chown -R nomad:nomad /opt/nomad
 
 # Add the Vault CA
 sudo /tmp/terraform-aws-vault/modules/update-certificate-store/update-certificate-store --cert-file-path /opt/vault/tls/ca.crt.pem
