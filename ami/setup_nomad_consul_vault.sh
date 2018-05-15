4f496938c429c5d9f89f767ba783f6f81c5eb6fe
Bobby <karmma0@gmail.com>
2018-05-14 22:13:29 -0600

    Removed dnsmasq config

--- ami/setup_nomad_consul_vault.sh
+++ ami/setup_nomad_consul_vault.sh
@@ -7,16 +7,6 @@ set -e
 git clone --branch "${NOMAD_MODULE_VERSION}" https://github.com/hashicorp/terraform-aws-nomad.git /tmp/terraform-aws-nomad
 /tmp/terraform-aws-nomad/modules/install-nomad/install-nomad --version "${NOMAD_VERSION}"
 
-# Setup dnsmasq config
-sudo tee /etc/dnsmasq.d/10-consul << EOF
-# Enable forward lookup of the 'consul' domain:
-server=/consul/127.0.0.1#8600
-
-# Modify as appropriate to enable reverse DNS lookups for
-# common netblocks found in RFC 1918, 5735, and 6598:
-rev-server=10.10.0.0/16,127.0.0.1#8600
-EOF
-
 # Install Vault
 git clone --branch "${VAULT_MODULE_VERSION}" https://github.com/hashicorp/terraform-aws-vault.git /tmp/terraform-aws-vault
 /tmp/terraform-aws-vault/modules/install-vault/install-vault --version "${VAULT_VERSION}"
